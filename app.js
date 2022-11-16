a2d34ce327adad507b7374582f6fb97740e22d43
Michelle Ross <miross629@gmail.com>
2022-11-16 12:54:06 -0800

    Pause sign-up build in app.js and change course

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // get DOM Elements
 
-import { signInUser } from './fetch-utils.js';
+import { redirectIfLoggedIn, signInUser } from './fetch-utils.js';
 
 // sign in
 const signInForm = document.getElementById('sign-in');
@@ -11,12 +11,21 @@ const signInPassword = document.getElementById('sign-in-password');
 const signUpForm = document.getElementById('sign-up');
 
 // events
-// Connect sign in and sign up forms to supabase
+// Connect sign up and sign in forms to supabase
+
+// sign up
+
+// sign in
 signInForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const user = await signInUser(signInEmail.value, signInPassword.value);
-});
 
-// Redirect to /other-page on successful auth
-// Redirect to /other-page when page loads if user is authenticated
+    // Redirect to /other-page on successful auth
+    if (user) {
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+    // Redirect to /other-page when page loads if user is authenticated
+});
