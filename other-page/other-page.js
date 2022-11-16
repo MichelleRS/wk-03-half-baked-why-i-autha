5e4731672030e76c07d31b68196519e0220f0640
Michelle Ross <miross629@gmail.com>
2022-11-16 15:33:26 -0800

    User can logout once logged in

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,2 +1,12 @@
 // use checkAuth function to redirect is user not authenticated
+import { checkAuth, logout } from '../fetch-utils.js';
+
+checkAuth();
+
+// DOM Elements
+const logOutButton = document.getElementById('logout');
+
 // add event listener to the logout button and call logout
+logOutButton.addEventListener('click', async () => {
+    await logout();
+});
